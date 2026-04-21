6367bc330496ace3892b71ca1c483b64afc8a624
a01048275592-boop <a01048275592@gmail.com>
2026-04-21 12:02:40 +0900

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,7 @@
 import { defineConfig } from 'astro/config';
-import sitemap from '@astrojs/sitemap';
+import sitemap from '@astrojs/sitemap';  // ← 이 줄
 
 export default defineConfig({
   site: 'https://opsoult.com',
-  integrations: [sitemap()],
+  integrations: [sitemap()],  // ← 이 줄
 });
